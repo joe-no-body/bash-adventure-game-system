d3450655a57b67c19a304f0847d032ce8b3da2c0
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2021-01-05 11:16:37 -0800

    add test syntax to game

---
+++ game/game.sh
@@ -0,0 +1,14 @@
+syntax yell = verb::yell
+
+syntax look = verb::look
+syntax look OBJ = verb::look
+syntax look in OBJ = verb::look-inside
+syntax look at OBJ = verb::look
+
+syntax go OBJ = verb::go
+
+syntax take OBJ = verb::take
+syntax take the OBJ = verb::take
+
+syntax attack OBJ with OBJ = verb::attack
+
