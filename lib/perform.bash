ceeedcc117c4c56e5e70fc4d5020bdb4685120e2
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2025-05-08 09:40:17 -0700

    clarify the purpose of perform()

--- lib/perform.bash
+++ lib/perform.bash
@@ -1,5 +1,7 @@
-# perform executes the action handler for the parsed verb, direct object, and
-# indirect object.
+# perform executes the action handler function for the parsed verb, direct
+# object, and indirect object. The main utility of this function is to provide a
+# scope for the `verb`, `dobject`, and `iobject` locals so they'll be cleaned up
+# after the function terminates.
 perform() {
   local verb="$1" dobject="$2" iobject="$3"
   func? "$verb" || {
