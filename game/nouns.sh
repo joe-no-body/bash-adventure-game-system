0fbb2a87e7563bf8dcd40e915b0cccc010f5e34b
joe-no-body <20505941+joe-no-body@users.noreply.github.com>
2025-05-08 11:48:35 -0700

    add some sample nouns to the game

--- game/nouns.sh
+++ game/nouns.sh
@@ -6,3 +6,8 @@ nouns::define object::south -s south
 nouns::define object::south -s s
 nouns::define object::west -s west
 nouns::define object::west -s w
+
+# These nouns are currently unused and have no object init functions associated
+# with them.
+nouns::define object::troll -t the -a angry -s troll
+nouns::define object::sword -t the -s sword
